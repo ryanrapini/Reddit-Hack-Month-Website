b10a93e12d9ad34e3939d0a46085a3dea833c5db
Ryan Rapini <ryanrapini@gmail.com>
2012-02-08 21:13:00 -0500

    added placeholder files.

---
+++ sample/tutorials.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<style type="text/css">
+			body{
+				padding-top: 40px;
+				padding-left: 50px;
+				padding-right: 50px;
+			}
+		</style>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title>HackMonth 2012 - Home</title>
+		<link href="layout/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen" />
+		<script type="text/javascript" src="layout/bootstrap.js"></script>
+	</head>
+	<body>
+		<?php include('includes/header.include'); ?>
+		<div class="container-fluid">
+
+		<div class="hero-unit">
+			<p>Coming soon...</p>
+		</div>
+
+		</div>
+
+		<hr />
+			<p>Copyright &copy;2012 HackMonth</p>
+			<p>This website is open-source! Fork me on <a href="https://github.com/ryanrapini/Reddit-Hack-Month-Website">Github!</a>
+	</body>
+</html>						
