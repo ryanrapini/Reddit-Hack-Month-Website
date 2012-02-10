2340a8a2e671f09b63d2e13a4af4af9a792f6e72
Diogenes <danieljbwork@gmail.com>
2012-02-10 12:37:29 +0000

    Wrote the Get Involved page

--- sample/getinvolved.php
+++ sample/getinvolved.php
@@ -7,26 +7,99 @@
 				padding-left: 50px;
 				padding-right: 50px;
 			}
+			p
+			{
+				text-align: justify;
+				padding-left: 10px;
+			}
+			h3
+			{
+				padding-bottom: 5px;
+			}
 		</style>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-		<title>HackMonth 2012 - Home</title>
+		<title>HackMonth 2012 - Get Involved</title>
 		<?php include('includes/scripts.include'); ?>
 	</head>
 	<body>
 		<?php include('includes/header.include'); ?>
+
 		<div class="container-fluid">
+		<div class="row-fluid">
+		<br />
+		<h1>Get Involved</h1>
 
-		<div class="hero-unit">
-			<p>Coming soon...</p>
-			    <div class="progress progress-info
-				progress-striped active">
-				<div class="bar"
-				style="width: 50%;"></div>
-				</div>
+		
+		
+			<div class="span8">
+				<h3>Welcome to HackMonth</h3>
+				<p>
+					We dedicate a month to something that often takes a little extra motivation: starting and finishing a project. In order to begin your HackMonth project, you usually start with the following:
+					<ul>
+						<li>A project proposal.</li>
+						<li>A set of objectives.</li>
+						<li>A marking scheme whereby you can judge your progress and end result.</li>
+					</ul>
+				</p>
+				<p>
+					If you are developing something, your HackMonth challenge will be to create a <i>working prototype</i>. If you complete your project beyond that, so much the better, but the point of HackMonth is to drive creativity, productivity, and personal development - not to prohibit further work on your project.
+				</p>
+				<p>
+					If your project is to <i>learn a new skill</i>, your aim will typically be to cover the basics of whatever it is you're learning about. Of course some things can take <i>months</i> to learn, and <i>years</i> to master, but HackMonth can give you a decent starting point.
+				</p>
+				<h3>Your Project Proposal</h3>
+				<p>
+					A project proposal simply conveys the <i>gist</i> of what you want to achieve. This can come in the form of a title, or for more specificity, a title and a paragraph. An example title might be: <i>I want to develop a parody social network in which people pretend to live in various historical eras.</i>
+				</p>
+				<h3>What can I HackMonth?</h3>
+				<p>
+					Criteria for HackMonth projects are very inclusive, as long as they involve technological thinking. We expect that most projects will involve coding something, but this isn't a rule.
+				</p>
+				<p>
+					The following are more examples of acceptable project proposals:
+					<i>
+					<ul>
+						<li>I want to attach a webcam to a remote-control plane or helicopter, fly it over my city (possibly automating the flight paths), broadcast the streaming images/video online, and sell the ad space to fund more missions.</li>
+						<li>I want to develop software which generates fractals from user input.</li>
+						<li>I want to learn about genetic algorithms and try my hand at coding examples.</li>
+						<li>I want to develop my own operating system.</li>
+						<li>I want to make a Flash game about my favourite movie.</li>
+					</ul>
+					</i>
+				</p>
+				<h3>Community - HackMonth on reddit</h3>
+				<p>
+					Currently the <a href='http://reddit.com/r/hackmonth'>HackMonth subreddit</a> is the best place to go to discuss your ideas, progress, and questions. If you haven't been to reddit before, it is very simple to <a href='https://ssl.reddit.com/login'>create an account</a>.
+				</p>
+				<p>
+					To participate on reddit, please consider the following rules:
+					<ul>
+						<li><b>Most kinds of posts will be allowed.</b> You can share your new proposal at/before the beginning of HackMonth; you can let us know how you're doing part-way through; you can talk about something cool you've achieved or learned; you can ask for advice concerning HackMonth or your specific project, and much more.</li>
+						<li><b>Try not to clutter the place up with banality.</b> If you want to make advice animals or ragecomics, make a new subreddit for it. Relevant images such as screenshots or cool things you've found while working are <i>encouraged</i>, but forever alone faces will be subject to moderation.</li>
+						<li><b>Be excellent to each other.</b><br /><br />
+							<p><i>I play it cool, and dig all jive<br />
+							That's the reason I'm alive<br />
+							My motto as I live and learn<br />
+							Is: dig and be dug in return.</i></p>
+							There's no reason to make other people feel small if you don't understand or respect their chosen project. If you disagree with the validity of a project, ignore it.
+							That said, <i><b>constructive</b></i> criticism is encouraged, if it is geared towards helping the participant complete or improve their project.</li>
+					</ul>
+				</p>
+				<h3>The HackMonth schedule</h3>
+				<p>
+					<b>HackMonth takes place in August</b>, but if your own schedule means you'll miss out in HackMonth, there's no reason not to pick another month and just go crazy. You'll be more than welcome to post on the <a href='http://reddit.com/r/hackmonth'>HackMonth subreddit</a> at unusual times.
+				</p>
+				<h3>How else can I get involved?</h3>
+				<p>
+					The website developers are <a href='http://www.reddit.com/r/hackmonth/comments/pewsc/hack_month_website_development/'>looking for help</a>. You can also <a href='https://github.com/ryanrapini/Reddit-Hack-Month-Website'>fork the main project on GitHub</a>. Some HackMonth projects are <b><a href='http://www.reddit.com/r/hackmonth/comments/pciu4/anyone_interested_in_an_open_source_collaborative/'>collaborative</a></b>: if you have an idea for one, make a new thread on reddit and ask if anyone would be interested.
+				</p>
+			</div>
+			<div class="span4">
+			
+			</div>
 		</div>
-
 		</div>
-
 		<?php include('includes/footer.include'); ?>
+
 	</body>
 </html>						
