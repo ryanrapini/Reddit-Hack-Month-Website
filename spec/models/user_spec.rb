9260dda7c23482a00909d12a6aa2296ccf284f65
Brian <brian.pina@gmail.com>
2012-02-10 00:42:11 -0500

    added in user spec. started authentication method shell

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "authentication" do
+    it "requires a username" do        
+        User.authenticate(nil, 'test').should == nil
+    end
+
+    it "requires a password" do
+        User.authenticate('user', nil).should == nil
+    end
+  end
 end
