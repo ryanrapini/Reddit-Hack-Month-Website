b216bd6faad35967a5524bf2bdf030c67bd83f47
Brian <brian.pina@gmail.com>
2012-02-10 00:06:51 -0500

    generated user model

---
+++ db/migrate/20120210050627_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :user_group_id
+
+      t.timestamps
+    end
+  end
+end
